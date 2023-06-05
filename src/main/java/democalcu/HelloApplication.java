30960eafae1a4a8862f92d8fbb3e6d490d051a75
Shakera Ema <shakerajannatema8685@gmail.com>
2023-06-05 21:13:08 +0600

    This is a new Project

--- src/main/java/democalcu/HelloApplication.java
+++ src/main/java/democalcu/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Calculator!");
+        Scene scene = new Scene(fxmlLoader.load(), 300, 500);
+        stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/democalcu/HelloController.java
+++ src/main/java/democalcu/HelloController.java
@@ -1,20 +1,46 @@
 package democalcu;
 
-/*import java.net.URL;
+import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.ResourceBundle.Control;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.Initializabele;
-*  */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
 
 /*import javafx.fxml.FXML;
 import javafx.scene.control.Label; */
 
-public class HelloController /* implements Initializable */  {
+public class HelloController  implements Initializable {
     /*@FXML
-    private Label welcomeText;
+    private Label welcomeText;*/
 
-    @FXML
+       @FXML
+    private TextField txt_result;
+       String op = "";
+       long number1;
+       long number2;
+       //click CTRL
+
+       public void Number(ActionEvent ae)
+       {
+
+       }
+    public void Operation (ActionEvent ae)
+    {
+
+    }
+
+
+    /*@FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to Calculator!");
     }*/
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        //
+    }
 }
